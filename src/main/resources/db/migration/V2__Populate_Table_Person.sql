9b25e2ca2b5587414767bdbd7499586686017246
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-12-08 14:48:32 -0300

    Migrations com Flyway via plugin no POM

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person` VALUES 
+ (2,'Campinas - SP, Brazil','Marisa','female','dos Santos Amaral'),
+ (4,'Amparo - SP, Brazil','Vilma','female','Teresa Pinto de Oliveira Assulfi'),
+ (6,'Osasco - SP, Brazil','Flavio','male','Cavalcante'),
+ (7,'Osasco - SP, Brazil','Flavio','male','Cavalcante');
