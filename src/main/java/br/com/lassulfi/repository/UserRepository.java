5c4d05c7b6df1b698e8053c069f8ab4cc221ba88
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-12-19 07:30:13 -0300

    Iniciada a implementacao do servico de autenticacao usando JWT

---
+++ src/main/java/br/com/lassulfi/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package br.com.lassulfi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import br.com.lassulfi.data.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	@Query("SELECT u FROM User u WHERE u.username =:username")
+	User findByUsername(@Param("username") String username);
+}
