62e24162d502c42420d447c9cb2e508824f17015
Luis Daniel Assulfi <luis.assulfi@sensedia.com>
2019-12-19 10:37:48 -0300

    Implementado repositorio e servico para autenticacao de usuarios

--- src/main/java/br/com/lassulfi/service/UserService.java
+++ src/main/java/br/com/lassulfi/service/UserService.java
@@ -1,8 +1,31 @@
 package br.com.lassulfi.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-@Service
-public class UserService {
+import br.com.lassulfi.repository.UserRepository;
 
+@Service
+public class UserService implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	public UserService(UserRepository userRepository) {
+		super();
+		this.userRepository = userRepository;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		var user = userRepository.findByUsername(username);
+		if(user != null) {
+			return user;
+		} else {
+			throw new UsernameNotFoundException("Username " + username + " not found");
+		}
+	}
 }
