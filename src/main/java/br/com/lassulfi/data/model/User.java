f954e201f133dbb568b2ae436e1d4f5f2f9f0d85
Luis Daniel Assulfi <luis.assulfi@sensedia.com>
2019-12-24 08:43:29 -0300

    Ajustes na entidade de usuarios

--- src/main/java/br/com/lassulfi/data/model/User.java
+++ src/main/java/br/com/lassulfi/data/model/User.java
@@ -8,6 +8,9 @@ import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
@@ -17,9 +20,14 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 public class User implements UserDetails, Serializable {
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
 	
 	@Column(name = "user_name", unique = true)
 	private String username;
