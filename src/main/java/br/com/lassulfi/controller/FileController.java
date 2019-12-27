32d2157033178976dad8c2df80596e4189949716
Luis Daniel Assulfi <luis.assulfi@sensedia.com>
2019-12-27 16:10:26 -0300

    Implementado upload e download e arquivos

--- src/main/java/br/com/lassulfi/controller/FileController.java
+++ src/main/java/br/com/lassulfi/controller/FileController.java
@@ -1,6 +1,20 @@
 package br.com.lassulfi.controller;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,6 +30,8 @@ import io.swagger.annotations.Api;
 @RestController
 @RequestMapping("/api/file/v1")
 public class FileController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(FileController.class);
 
 	@Autowired
 	private FileStorageService fileStorageService;
@@ -25,10 +41,39 @@ public class FileController {
 		String filename = fileStorageService.saveFile(file);
 		
 		String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
-				.path("/api/file/v1/download")
+				.path("/api/file/v1/download/")
 				.path(filename)
 				.toUriString();
 		
 		return new UploadFileResponseVO(filename, fileDownloadUri, file.getContentType(), file.getSize());
+	}
+	
+	@PostMapping("/multi-files/upload")
+	public List<UploadFileResponseVO> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
+		return Arrays.asList(files)
+				.stream()
+				.map(file -> uploadFile(file))
+				.collect(Collectors.toList());
+	}
+	
+	@GetMapping("/download/{filename:.+}")
+	public ResponseEntity<Resource> downloadFile(@PathVariable String filename, HttpServletRequest request) {
+		Resource resource = fileStorageService.loadFileAsResource(filename);
+		
+		String contentType = null;
+		try {
+			contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+		} catch (Exception e) {
+			logger.info("Couldn't indentify file type. ");
+		}
+		
+		if(contentType == null) {
+			contentType = "application/octet-stream";
+		}
+		
+		return ResponseEntity.ok()
+				.contentType(MediaType.parseMediaType(contentType))
+				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+				.body(resource);
 	}
 }
